1d4b106f469449bb4552a2ba3760bbda7b8a8746
HatsuneMona <songmiao39@foxmail.com>
2022-12-06 10:54:30 +0800

    feat (config): 配置文件 增加Jwt刷新时间

--- config/jwt.go
+++ config/jwt.go
@@ -4,4 +4,5 @@ type Jwt struct {
 	Secret                  string `mapstructure:"secret" json:"secret" yaml:"secret"`
 	JwtTtl                  int    `mapstructure:"jwt_ttl" json:"jwt_ttl" yaml:"jwt_ttl"`
 	JwtBlacklistGracePeriod int    `mapstructure:"jwt_blacklist_grace_period" json:"jwt_blacklist_grace_period" yaml:"jwt_blacklist_grace_period"` // jwt过期宽限时间
+	RefreshGracePeriod      int64  `mapstructure:"refresh_grace_period" json:"refresh_grace_period" yaml:"refresh_grace_period"`                   // token 自动刷新宽限时间（秒）
 }
